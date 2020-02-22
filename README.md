819242705ac849054cb0d8f51dd8eed9af889259
rocky1991 <hongchuan91@gmail.com>
2020-02-22 10:10:18 -0700

    add dissertation and syllabus

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is the final project of Machine Learning class.
 
-Need to install ```pip3 install networkx```
+# Need to install 
+	### networkx for graph visualization ```pip3 install networkx```
+	### matplotlib for creating graph ```pip3 install matplotlib```
--- useful_scripts/graph_visualization.py
+++ useful_scripts/graph_visualization.py
@@ -1,5 +1,5 @@
 import networkx as nx 
-
+import matplotlib.pyplot as plt
 
 # this function reads from a file the graph information
 # currently a dummy file
@@ -9,9 +9,14 @@ def read_edges(file_path):
 
 	return [(1,2), (1,6), (2,3)]
 
+def read_nodes(file_path):
+
+	return [1,2,3,6]
 
 edges = read_edges("dummy_path")
-
+G = nx.Graph()
 G.add_edges_from(edges)
 nx.draw_networkx(G, with_label = True)
+
+plt.show()
 
